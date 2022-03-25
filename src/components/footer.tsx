aa2b0c5639ade222d43a2afc5ac7ede43d6eed3b
baosonnguyendang <baosonnguyendang@gmail.com>
2022-03-26 00:10:11 +0700

    feat: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Footer = (props: any) => {
+  return (
+    <div>
+      <div className="footer-1">
+        <div className="footer-1-a">mplify</div>
+        <div className="footer-1-b">
+          <div className="logo li"></div>
+          <div className="logo fb"></div>
+          <div className="logo in"></div>
+        </div>
+      </div>
+      <div className="footer-2">
+        <span>MPLIFY VIETNAM COMPANY LIMITED. </span>
+        <span>All right reserved.</span>
+      </div>
+    </div>
+  );
+};
+
+Footer.propTypes = {};
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const header = (props: any) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+// header.propTypes = {
+    
+// };
+
+export default header;
